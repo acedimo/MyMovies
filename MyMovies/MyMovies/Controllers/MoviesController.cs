342080e03096ac1843d66904064d8dc423f16b0b
dimovski.aleksandar3@gmail.com <dimovski.aleksandar3@gmail.com>
2021-03-24 13:35:53 +0100

    Intro to MVC

---
+++ MyMovies/MyMovies/Controllers/MoviesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMovies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMovies.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var movie1 = new Movie() 
+            {
+                Id = 1,
+                Title = "Batman",
+                Duration = 120,
+                Description = "asadasdadasd",
+            };
+
+            var movie2 = new Movie()
+            {
+                Id = 1,
+                Title = "Batman",
+                Duration = 120,
+                Description = "asadasdadasd",
+            };
+
+            var movies = new List<Movie> { movie1, movie2 };
+
+            return View(movies);
+        }
+    }
+}
--- MyMovies/MyMovies/Startup.cs
+++ MyMovies/MyMovies/Startup.cs
@@ -50,7 +50,7 @@ namespace MyMovies
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Movies}/{action=Index}/{id?}");
             });
         }
     }
